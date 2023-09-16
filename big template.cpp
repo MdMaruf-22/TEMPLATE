33b5c781be96b08814ce98ae0971adec7e5b4e74
Mohammed Maruf Islam <64244354+MdMaruf-22@users.noreply.github.com>
2023-09-16 23:42:39 +0600

    Update big template.cpp

--- big template.cpp
+++ big template.cpp
@@ -1,65 +1,77 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define mod 1000000007
 
-bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
-
-ll gcd(ll a,ll b)
+bool prime(ll a)
 {
-    if(b==0) return a;
-    if(b>a) return gcd(b,a);
-    return gcd(b,a%b);
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
 }
 
-ll lcm(ll a,ll b)
+ll gcd(ll a, ll b)
 {
-    return (a*b)/gcd(a,b);
+    if (b == 0)
+        return a;
+    if (b > a)
+        return gcd(b, a);
+    return gcd(b, a % b);
 }
-ll power(ll x,  ll y)
+
+ll lcm(ll a, ll b)
+{
+    return (a * b) / gcd(a, b);
+}
+ll power(ll x, ll y)
 {
     if (y == 0)
         return 1;
     else if (y % 2 == 0)
-        return ((power(x, y / 2)%mod) * (power(x, y / 2)%mod))%mod;
+        return ((power(x, y / 2) % mod) * (power(x, y / 2) % mod)) % mod;
     else
-        return ((((x  %mod) * ((power(x, y / 2)%mod)))%mod * (power(x, y / 2)%mod))%mod)%mod;
+        return ((((x % mod) * ((power(x, y / 2) % mod))) % mod * (power(x, y / 2) % mod)) % mod) % mod;
 }
-ll combi(ll n,ll k)
+int nCr(int n, int r)
 {
-    ll ans=1;
-    k=k>n-k?n-k:k;
-    int j=1;
-    for(;j<=k;j++,n--)
-    {
-        if(n%j==0)
-        {
-            ans*=n/j;
-        }else
-        if(ans%j==0)
-        {
-            ans=ans/j*n;
-        }else
-        {
-            ans=(ans*n)/j;
+    long long p = 1, k = 1;
+    if (n - r < r)
+        r = n - r;
+    if (r != 0) {
+        while (r) {
+            p *= n;
+            k *= r;
+            long long m = __gcd(p, k);
+            p /= m;
+            k /= m;
+            n--;
+            r--;
         }
     }
-    return ans;
+    else
+        p = 1;
+    return p;
 }
 
 void solve()
 {
-
+    ll n, m;
+    cin >> n >> m;
+    cout << power(n, m);
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--)
+    {
         solve();
     }
     return 0;
 }
-
