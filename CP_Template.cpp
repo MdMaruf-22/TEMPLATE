9a076a15a455ec4603d645b5d2806922b82a2fe3
Mohammed Maruf Islam <64244354+MdMaruf-22@users.noreply.github.com>
2023-10-21 19:49:17 +0600

    Updated the prime to sieve

--- CP_Template.cpp
+++ CP_Template.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long
-#define mod 1000000007
 
-bool prime(ll a)
-{
-    if (a == 1)
-        return 0;
-    for (int i = 2; i <= round(sqrt(a)); ++i)
-        if (a % i == 0)
-            return 0;
-    return 1;
+#define int long long
+const int mod = 1e9;
+
+bool is_prime(int a) {
+    if (a <= 1) {
+        return false;
+    }
+    if (a <= 3) {
+        return true;
+    }
+    if (a % 2 == 0 || a % 3 == 0) {
+        return false;
+    }
+    for (int i = 5; i * i <= a; i += 6) {
+        if (a % i == 0 || a % (i + 2) == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-ll gcd(ll a, ll b)
-{
+int gcd(int a, int b) {
     if (b == 0)
         return a;
     if (b > a)
@@ -22,12 +30,11 @@ ll gcd(ll a, ll b)
     return gcd(b, a % b);
 }
 
-ll lcm(ll a, ll b)
-{
+int lcm(int a, int b) {
     return (a * b) / gcd(a, b);
 }
-ll power(ll x, ll y)
-{
+
+int power(int x, int y) {
     if (y == 0)
         return 1;
     else if (y % 2 == 0)
@@ -35,8 +42,8 @@ ll power(ll x, ll y)
     else
         return ((((x % mod) * ((power(x, y / 2) % mod))) % mod * (power(x, y / 2) % mod)) % mod) % mod;
 }
-int nCr(int n, int r)
-{
+
+int nCr(int n, int r) {
     long long p = 1, k = 1;
     if (n - r < r)
         r = n - r;
@@ -50,27 +57,21 @@ int nCr(int n, int r)
             n--;
             r--;
         }
-    }
-    else
+    } else
         p = 1;
     return p;
 }
 
-void solve()
-{
-    ll n, m;
-    cin >> n >> m;
-    cout << power(n, m);
+void solve() {
+    
 }
 
-int main()
-{
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--)
-    {
+    int t = 1;
+    // cin >> t;
+    while (t--) {
         solve();
     }
     return 0;
